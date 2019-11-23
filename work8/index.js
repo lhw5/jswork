fe72dd91d443a67cdc286b5cead34e1147c4c6c0
lhw5 <203658975@qq.com>
2019-11-23 18:34:46 +0800

    work8

--- work8/index.js
+++ work8/index.js
@@ -1,7 +1,7 @@
 function calc(func){
     let result = document.getElementById('result')
-    let num1 = parseInt(document.getElementById('num1').value)
-    let num2 = parseInt(document.getElementById('num2').value)
+    let num1 = parseFloat(document.getElementById('num1').value)
+    let num2 = parseFloat(document.getElementById('num2').value)
     if(isNaN(num1) || isNaN(num2)){
         alert('请输入数字')
         return false
