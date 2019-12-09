78212bf3501b3178a5e38f79af5e356683b90eb2
lhw5 <203658975@qq.com>
2019-12-09 23:21:07 +0800

    work15

--- work15/ShopCart.js
+++ work15/ShopCart.js
@@ -1,9 +1,10 @@
-(function (window) {
+(function(window){
     /**
-     * @param {String} prefix 前缀
-     * @param {Array} defCart 初始数据  [{name；'',price:1,num: 1}]
+     * ShopCart购物车
+     * @param {String} prefix 前端
+     * @param {Array} defCart 初始数据 [{name: '', price:1,num:1}]
      */
-    let ShopCart = function (prefix, defCart) {
+    let ShopCart = function(prefix, defCart) {
         Find.prototype.prefix = prefix;
         let cart = new Cart(document.getElementsByClassName(prefix)[0]);
         for (let i in defCart) {
@@ -16,7 +17,7 @@
      * @constructor
      * @param {Object} obj 购物车容器对象
      */
-    function Cart(obj) {
+    function Cart(obj){
         this.items = [];
         let find = new Find(obj);
         this.all = find.className('all');
@@ -27,26 +28,24 @@
         this.tmp = find.className('item');
         this.tmp.parentNode.removeChild(this.tmp);
         let cart = this;
-        //为全选添加单击事件
-        this.all.onclick = function () {
-            cart. checkAll();
+        this.all.onclick = function() {
+            cart.checkAll();
         };
-        //全不选添加单击事件
-        this.unall.onclick = function () {
+        this.unall.onclick = function(){
             cart.uncheckAll();
         }
     }
     Cart.prototype = {
         /**
          * 向购物车中添加商品
-         * @param {Object} data 商品信息 
+         * @param {Object} data 商品信息
          */
-        add: function (data) {
+        add: function(data) {
             let tmp = this.tmp.cloneNode(true);
             let item = new Item(tmp, data);
             let cart = this;
             //勾选
-            item.check.onclick = function () {
+            item.check.onclick = function() {
                 cart.updateTotal();
             };
             //增加数量
@@ -56,18 +55,18 @@
                 cart.updateTotal();
             };
             //减少数量
-            item.reduce.onclick = function () {
-                if (item.data.num > 1) {
+            item.reduce.onclick = function() {
+                if (item.data.num>1){
                     item.num.textContent = --item.data.num;
                     item.updateSubtotal();
                     cart.updateTotal();
-                } else {
+                }else {
                     alert('至少选择1件，如果不需要，请直接删除');
                 }
             };
-            // 删除商品
-            item.del.onclick = function () {
-                if (confirm('您确定要删除此商品吗？')) {
+            //删除商品
+            item.del.onclick = function (){
+                if(confirm('您确定要删除此商品吗？')){
                     tmp.parentNode.removeChild(tmp);
                     cart.del(item);
                     cart.updateTotal();
@@ -80,22 +79,25 @@
             //放入购物车容器中
             this.bottom.before(tmp);
         },
-        del: function (item) {
-            for (let i in this.items) {
-                if (this.items[i] === item) {
-                    delete this.items[i];
+        /**
+         * 删除保存的商品对象
+         * @param {Object} item 待删除的商品对象
+         */
+        del: function(item){
+            for(let i in this.items){
+                if(this.items[i]===item){                
+                delete this.items[i];
                 }
             }
         },
-
         /**
-         * 更新总计
-         */
-        updateTotal: function () {
-            let num = 0, price = 0;
-            for (let i in this.items) {
+        * 更新总计
+        */
+        updateTotal: function(){
+            let num = 0 , price = 0;
+            for (let i in this.items){
                 let item = this.items[i];
-                if (item.check.checked) {
+                if(item.check.checked){
                     num += item.data.num;
                     price += item.data.num * item.data.price;
                 }
@@ -104,26 +106,31 @@
             this.price.textContent = price.toFixed(2);
         },
         /**
-         *全选
+         * 全选
          */
-        checkAll: function () {
-            for (let i in this.items) {
+        checkAll:function(){
+            for(let i in this.items){
                 this.items[i].check.checked = true;
             }
             this.updateTotal();
         },
         /**
-         *全不选
+         * 全不选
          */
-        uncheckAll: function () {
-            for (let i in this.items) {
-                this.itens[i].check.checked = false;
+        uncheckAll:function(){
+            for(let i in this.items){
+                this.items[i].check.checked = false;
             }
             this.updateTotal();
         }
     };
-
-    function Item(tmp, data) {
+    /**
+     * Item 购物车中的单件商品
+     * @constructor
+     * @param {String} tmp 模板
+     * @param {Array} data 数据
+     */
+    function Item(tmp, data){
         let find = new Find(tmp);
         this.check = find.className('check');
         this.name = find.className('name');
@@ -135,32 +142,32 @@
         this.del = find.className('del');
         this.data = data;
         this.name.textContent = data.name;
-        this.price, textContent = data.price.toFixed(2);
+        this.price.textContent = data.price.toFixed(2);
         this.num.textContent = data.num;
     }
     Item.prototype = {
         /**
-         *更新小计
+         * 更新小计
          */
-        updateSubtotal: function () {
+        updateSubtotal:function() {
             this.subtotal.textContent = (this.data.num * this.data.price).toFixed(2);
         }
     };
     /**
      * Find 查找器
-     *  @constructor
-     *  @param {Object} obj 待查找对象所在容器
+     * @constructor
+     * @param {Object} obj 待查找对象所在容器
      */
-    function Find(obj) {
+    function Find(obj){
         this.obj = obj;
     }
     Find.prototype = {
-        prefix: '',
+        prefix:'',
         /**
-         * 按照class查找元素
+         * 按照 class查找元素
          * @param {string} className
          */
-        className: function (className) {
+        className:function(className){
             return this.obj.getElementsByClassName(this.prefix + '-' + className)[0];
         }
     };
